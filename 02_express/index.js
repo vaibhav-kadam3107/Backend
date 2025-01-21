f2ca6d1c018a4fb857536eaf310ad1132de58fd3
vaibhav-kadam3107 <vaibhav.kadam21@vit.edu>
2025-01-21 13:15:34 +0530

    Add logging functionality with Winston and Morgan to the Express app

--- 02_express/index.js
+++ 02_express/index.js
@@ -1,10 +1,30 @@
 import 'dotenv/config'
 import express from 'express' 
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express() 
 const port = process.env.PORT || 3000 
 
-app.use(express.json())  // For parsing application/json
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
+app.use(express.json())  
 
 let usersData = [] 
 let id = 1 
